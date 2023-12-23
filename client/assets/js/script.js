33694018b5ca55fa370921de560f70dc21526f97
Artur Bomtempo Colen <arturbcolen@gmail.com>
2023-12-23 17:47:56 -0300

    feat(client): addition of the function to change the theme of the page and animations

--- client/assets/js/script.js
+++ client/assets/js/script.js
@@ -14,7 +14,7 @@ if (navClose) {
     });
 }
 
-const navLink = document.querySelectorAll('nav-link');
+const navLink = document.querySelectorAll('.nav-link');
 
 function linkAction() {
     const navMenu = document.getElementById('nav-menu');
@@ -32,7 +32,7 @@ function scrollHeader() {
 
 window.addEventListener('scroll', scrollHeader);
 
-let newSwiper = new Swiper('.new-swiper', {
+let newSwiper = new Swiper('.actions-swiper', {
     spaceBetween: 24,
     loop: 'true',
     slidesPerView: 'auto',
@@ -77,3 +77,38 @@ function scrollUp() {
 }
 
 window.addEventListener('scroll', scrollUp);
+
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'bx-sun';
+
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun';
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+    themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme);
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+    localStorage.setItem('selected-theme', getCurrentTheme());
+    localStorage.setItem('selected-icon', getCurrentIcon());
+});
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400
+});
+
+sr.reveal(`.home-image, .actions-container`);
+sr.reveal(`.home-data`, { delay: 500 });
+sr.reveal(`.wishes-content`, { interval: 100 });
+sr.reveal(`.message-data`, { origin: 'left' });
+sr.reveal(`.message-image`, { origin: 'right' });
